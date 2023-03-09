6571aeb3c3e7c9bdfd406afa5f295ab126fe3781
Thiago Prates <tsprates@gmail.com>
2023-03-09 19:01:25 -0300

    adiciona league csv para importação de pacientes

--- api/app/Jobs/ImportaPacientesCsv.php
+++ api/app/Jobs/ImportaPacientesCsv.php
@@ -4,54 +4,53 @@ namespace App\Jobs;
 
 use App\Http\Requests\StorePacienteRequest;
 use App\Models\Paciente;
+use Exception;
 use Illuminate\Bus\Queueable;
-use Illuminate\Contracts\Queue\ShouldBeUnique;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Validator;
+use League\Csv\Reader;
 
 class ImportaPacientesCsv implements ShouldQueue
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
 
-    private $csvFile;
+    private $csvFilePath;
 
-    /**
-     * Create a new job instance.
-     *
-     * @return void
-     */
-    public function __construct($csvFile)
+    public function __construct($csvFileName)
     {
-        $this->csvFile = storage_path('app/public/csv/' . $csvFile);
+        $this->csvFilePath = storage_path('app/public/csv/' . $csvFileName);
     }
 
-    /**
-     * Execute the job.
-     *
-     * @return void
-     */
     public function handle()
     {
-        if (!is_file($this->csvFile)) {
-            Log::error(sprintf('%s não é um csv válido.', $this->csvFile));
-            return;
-        }
+        try {
+            $csv = Reader::createFromPath($this->csvFilePath, 'r');
+            $csv->setHeaderOffset(0);
+            $header = array_map('trim', $csv->getHeader());
 
-        $rules = (new StorePacienteRequest())->rules();
-        $file = fopen($this->csvFile, 'r');
-        $fields = fgetcsv($file);
-        while (($data = fgetcsv($file)) !== false) {
-            $input = array_combine($fields, $data);    
-            $validator = Validator::make($input, $rules);
-            if ($validator->fails()) {
+            $rules = (new StorePacienteRequest())->rules();
+            
+            // TODO:
+            unset($rules['foto']);
+
+            foreach ($csv->getRecords() as $record) {
+                $input = array_combine($header, $record);
+                $validator = Validator::make($input, $rules);
+
+                if ($validator->fails()) {
+                    Log::info(json_encode([$record, $validator->errors()]));
+                    continue;
+                }
+                
                 $paciente = Paciente::create($input);
                 $paciente->endereco()->create($input);
             }
+        } catch (Exception $exception) {
+            Log::error($exception->getMessage());
         }
-        fclose($file);
     }
 }
